6d021af5ce01c80ea6dafef2842a22dd830a1e99
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-10 19:15:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_holidays_with_bbq(holiday_hash)
   bbq_holidays = []
   holiday_hash.each do |season, data|
     binding.pry
-    if holiday_hash.dig(season, data).include?("BBQ")
+    if holiday_hash.dig(season, data.keys).include?("BBQ")
       bbq_holidays << season
     end
   end
